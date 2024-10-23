09632863a521b6c46451ce2aee70288133b6995a
Nikita Plotnikov 221RDB021 <67636595+Gallenmar@users.noreply.github.com>
2024-10-23 20:58:51 +0300

    Revert "tabs to spaces"
    This reverts commit 02a7edf2b17a21eb3155f0db718a707f8bb4da61.

--- app/context/TaskContext.tsx
+++ app/context/TaskContext.tsx
@@ -1,49 +1,49 @@
-'use client';
+"use client";
 
-import { createContext, FC, ReactNode, useContext, useState } from 'react';
-import tasksData from '../tasks.json';
+import { createContext, FC, ReactNode, useContext, useState } from "react";
+import tasksData from "../tasks.json";
 export interface Task {
-  id: string;
-  title: string;
-  description: string | null;
-  status: 'open' | 'in-progress' | 'closed' | null;
-  type: 'feature' | 'bug' | 'enhancement' | 'task' | null;
+	id: string;
+	title: string;
+	description: string | null;
+	status: "open" | "in-progress" | "closed" | null;
+	type: "feature" | "bug" | "enhancement" | "task" | null;
 }
 
 interface TaskContextType {
-  tasks: Task[];
-  addTask: (newTask: Task) => void;
-  deleteTask: (taskId: string) => void;
+	tasks: Task[];
+	addTask: (newTask: Task) => void;
+	deleteTask: (taskId: string) => void;
 }
 
 const TaskContext = createContext<TaskContextType | undefined>(undefined);
 
 export const TaskContextProvider: FC<{ children: ReactNode }> = ({
-  children
+	children,
 }) => {
-  const [tasks, setTasks] = useState<Task[]>(tasksData as Task[]);
+	const [tasks, setTasks] = useState<Task[]>(tasksData as Task[]);
 
-  const addTask = (newTask: Task) => {
-    setTasks((prevTasks) => [...prevTasks, newTask]);
-  };
+	const addTask = (newTask: Task) => {
+		setTasks((prevTasks) => [...prevTasks, newTask]);
+	};
 
-  const deleteTask = (taskId: string) => {
-    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
-  };
+	const deleteTask = (taskId: string) => {
+		setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+	};
 
-  return (
-    <TaskContext.Provider value={{ tasks, addTask, deleteTask }}>
-      {children}
-    </TaskContext.Provider>
-  );
+	return (
+		<TaskContext.Provider value={{ tasks, addTask, deleteTask }}>
+			{children}
+		</TaskContext.Provider>
+	);
 };
 
 export function useTaskContext(): TaskContextType {
-  const task = useContext(TaskContext);
+	const task = useContext(TaskContext);
 
-  if (!task) {
-    throw new Error('No task provided to the useContext');
-  }
+	if (!task) {
+		throw new Error("No task provided to the useContext");
+	}
 
-  return task;
+	return task;
 }
--- app/theme.ts
+++ app/theme.ts
@@ -1,65 +1,65 @@
-'use client';
-import { grey } from '@mui/material/colors';
-import { alpha, createTheme } from '@mui/material/styles';
+"use client";
+import { grey } from "@mui/material/colors";
+import { alpha, createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-  typography: {
-    fontFamily: 'var(--font-roboto)'
-  },
-  palette: {
-    primary: {
-      main: '#9055A2'
-    },
-    secondary: {
-      main: '#2c143a'
-    },
-    mode: 'dark'
-    // colors for future
-    //D499B9
-    //E8C1C5
-  },
-  components: {
-    MuiAppBar: {
-      styleOverrides: {
-        root: {
-          backgroundColor: '#2c143a'
-        }
-      }
-    },
-    MuiCard: {
-      styleOverrides: {
-        root: ({ theme }) => {
-          return {
-            padding: 16,
-            gap: 16,
-            transition: 'all 100ms ease',
-            backgroundColor: grey[50],
-            borderRadius: theme.shape.borderRadius,
-            border: `1px solid ${theme.palette.divider}`,
-            boxShadow: 'none',
-            ...theme.applyStyles('dark', {
-              backgroundColor: grey[800]
-            }),
-            variants: [
-              {
-                props: {
-                  variant: 'outlined'
-                },
-                style: {
-                  border: `1px solid ${theme.palette.divider}`,
-                  boxShadow: 'none',
-                  background: 'hsl(0, 0%, 100%)',
-                  ...theme.applyStyles('dark', {
-                    background: alpha(grey[900], 0.4)
-                  })
-                }
-              }
-            ]
-          };
-        }
-      }
-    }
-  }
+	typography: {
+		fontFamily: "var(--font-roboto)",
+	},
+	palette: {
+		primary: {
+			main: "#9055A2",
+		},
+		secondary: {
+			main: "#2c143a",
+		},
+		mode: "dark",
+		// colors for future
+		//D499B9
+		//E8C1C5
+	},
+	components: {
+		MuiAppBar: {
+			styleOverrides: {
+				root: {
+					backgroundColor: "#2c143a",
+				},
+			},
+		},
+		MuiCard: {
+			styleOverrides: {
+				root: ({ theme }) => {
+					return {
+						padding: 16,
+						gap: 16,
+						transition: "all 100ms ease",
+						backgroundColor: grey[50],
+						borderRadius: theme.shape.borderRadius,
+						border: `1px solid ${theme.palette.divider}`,
+						boxShadow: "none",
+						...theme.applyStyles("dark", {
+							backgroundColor: grey[800],
+						}),
+						variants: [
+							{
+								props: {
+									variant: "outlined",
+								},
+								style: {
+									border: `1px solid ${theme.palette.divider}`,
+									boxShadow: "none",
+									background: "hsl(0, 0%, 100%)",
+									...theme.applyStyles("dark", {
+										background: alpha(grey[900], 0.4),
+									}),
+								},
+							},
+						],
+					};
+				},
+			},
+		},
+	},
 });
 
 export default theme;
