02a7edf2b17a21eb3155f0db718a707f8bb4da61
Nikita Plotnikov 221RDB021 <67636595+Gallenmar@users.noreply.github.com>
2024-10-23 20:57:26 +0300

    tabs to spaces

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,36 +1,36 @@
-import React from "react";
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
-import Image from "next/image";
-import Link from "next/link";
-import AddButton from "./addButton";
+import React from 'react';
+import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
+import AddButton from './addButton';
 
 const Navbar: React.FC = () => {
-	return (
-		<AppBar position="static">
-			<Container maxWidth="lg">
-				<Toolbar
-					sx={{
-						display: "flex",
-						justifyContent: "space-between",
-						alignItems: "center",
-					}}
-				>
-					<Link href="/" style={{ display: "flex", alignItems: "center" }}>
-						<Image
-							src="/logo.png"
-							alt="Task Manager Logo"
-							width={40}
-							height={40}
-						/>
-						<Typography variant="h6" sx={{ marginLeft: 1 }}>
-							Task Manager
-						</Typography>
-					</Link>
-					<AddButton />
-				</Toolbar>
-			</Container>
-		</AppBar>
-	);
+  return (
+    <AppBar position="static">
+      <Container maxWidth="lg">
+        <Toolbar
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}
+        >
+          <Link href="/" style={{ display: 'flex', alignItems: 'center' }}>
+            <Image
+              src="/logo.png"
+              alt="Task Manager Logo"
+              width={40}
+              height={40}
+            />
+            <Typography variant="h6" sx={{ marginLeft: 1 }}>
+              Task Manager
+            </Typography>
+          </Link>
+          <AddButton />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Navbar;
